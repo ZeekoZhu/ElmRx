66c6eae4023590090aacb4fca359685331326997
Zeeko <vaezt@outlook.com>
2018-06-19 17:45:57 +0800

    export msg creator

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { ElmArch } from './src/ElmRx';
 import { ElmArchService } from './src/ElmServices';
+import { msgOf } from './src/ElmRxMsg';
 
-export { ElmArch, ElmArchService };
+export { ElmArch, ElmArchService, msgOf };
